0341ba572c98d25c878353b9f07aaeb2e3251d45
Suyoung <jsy8492@gmail.com>
2023-04-17 21:29:25 +0900

    refactor: useRestaurants custom hook의 음식점 데이터 페칭 로직을 분리

--- src/domain/hooks/useRestaurants.ts
+++ src/domain/hooks/useRestaurants.ts
@@ -1,8 +1,23 @@
-import { useEffect, useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { RESTAURANTS_LOCAL_STORAGE_KEY } from '../constants';
 import { getLocalStorage, setLocalStorage } from '../../utils/localStorage';
 import type { Restaurant } from '../type';
+
+const initRestaurants = async (
+  setRestaurants: Dispatch<SetStateAction<Restaurant[]>>,
+) => {
+  const response = await fetch('./mockData.json');
+
+  if (!response.ok) {
+    throw new Error('음식점 목록을 불러올 수 없습니다.');
+  }
+
+  const data = await response.json();
+
+  setRestaurants(data);
+  setLocalStorage(RESTAURANTS_LOCAL_STORAGE_KEY, data);
+};
 
 export const useRestaurants = () => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
@@ -17,20 +32,7 @@ export const useRestaurants = () => {
       return;
     }
 
-    const fetchRestaurants = async () => {
-      const response = await fetch('./mockData.json');
-
-      if (!response.ok) {
-        throw new Error('음식점 목록을 불러올 수 없습니다.');
-      }
-
-      const data = await response.json();
-
-      setRestaurants(data);
-      setLocalStorage(RESTAURANTS_LOCAL_STORAGE_KEY, data);
-    };
-
-    fetchRestaurants();
+    initRestaurants(setRestaurants);
   }, []);
 
   return { restaurants };
